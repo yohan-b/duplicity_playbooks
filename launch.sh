6d3e2eafa03d419690f4b7fadb4cf6947c159e55
yohan <783b8c87@scimetis.net>
2023-10-15 00:12:04 +0200

    Remove useless script call.

--- launch.sh
+++ launch.sh
@@ -6,7 +6,4 @@ SCRIPTPATH=$(dirname $SCRIPT)
 
 cd $SCRIPTPATH
 USER=$(whoami)
-/home/$USER/get_secrets.sh
-
-cd $SCRIPTPATH
 sudo -E docker run --net=host --rm -e KEY -e SECRETS_ARCHIVE_PASSPHRASE -v ~/repository/docker-duplicity-stack:/root/docker-duplicity-stack -v $SCRIPTPATH:/root/duplicity_playbooks -i ansible /root/duplicity_playbooks/script.sh
