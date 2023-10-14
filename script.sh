abe1b669c90e074c6b4b787cd14ebad985825550
yohan <783b8c87@scimetis.net>
2023-10-15 00:04:39 +0200

    Refactor

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /root/duplicity_playbooks
 ansible-galaxy collection install -r collections/requirements.yml
-source /root/openrc.sh
-ansible-playbook down.yml
+ansible-playbook playbook.yml
