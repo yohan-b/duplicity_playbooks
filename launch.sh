fed93ef0589f89c76cde9e5340be543e3e314eb9
yohan <783b8c87@scimetis.net>
2023-10-22 12:49:03 +0200

    Rename scripts.

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Absolute path to this script
-SCRIPT=$(readlink -f $0)
-#Absolute path this script is in
-SCRIPTPATH=$(dirname $SCRIPT)
-
-cd $SCRIPTPATH
-USER=$(whoami)
-sudo -E docker run --net=host --rm -e KEY -e DOC_KEY -e SECRETS_ARCHIVE_PASSPHRASE -e DUPLICITY_PASSPHRASE -e BACKUP_WORKFLOW -v $SCRIPTPATH:/root/duplicity_playbooks -i ansible /root/duplicity_playbooks/script.sh
